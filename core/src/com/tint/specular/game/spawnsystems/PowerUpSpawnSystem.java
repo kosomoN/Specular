df5f367d1b17b3ab08648d397bb7c4da747f2659
doc97 <danielviking@windowslive.com>
2014-04-24 22:38:23 +0300

    Reworked power-up spawnsystem

--- core/src/com/tint/specular/game/spawnsystems/PowerUpSpawnSystem.java
+++ core/src/com/tint/specular/game/spawnsystems/PowerUpSpawnSystem.java
@@ -36,51 +36,50 @@ public class PowerUpSpawnSystem extends SpawnSystem {
 		float x = rand.nextInt(gs.getCurrentMap().getWidth() - 100) + 50;
 		float y = rand.nextInt(gs.getCurrentMap().getHeight() - 100) + 50;
 		
-		/* First it adds a random factor and sets how common a powerup is, and lastly it takes in to account the affecting powerups
+		/* First it adds a random factor, affects 50% max, and it takes in to account the affecting powerups
 		 * Example:
-		 * Affecting powerups ~ 1f - player.getLife() / 4f; to get importance in percentage
-		 * How common ~ 0.7f; ranging from 0% to 100%
-		 * Random factor ~ rand.nextInt(10) / 10f; returning a float casted double
+		 * Affecting powerups ~ (1f - player.getLife() / 4f) * 0.5f; to get importance in percentage (max 50%)
+		 * Random factor ~ rand.nextDouble();
 		 */
 		float importance = 0;
 		Player player = gs.getPlayer();
-		float life = rand.nextInt(10) / 10f * 0.7f - (player.getLife() - 1) / 4f;
+		float life = (float) (rand.nextDouble() * 0.5f + (1 - player.getLife() / 4f) * 0.5f);
 		if(life > importance)
 			importance = life;
 		
-		float fireRate = rand.nextInt(10) / 10f * 0.8f - (10 - player.getFireRate()) / 10f;
+		float fireRate = (float) (rand.nextDouble() * 0.5f + player.getFireRate() / 10f * 0.5f);
 		if(fireRate > importance)
 			importance = fireRate;
 		
-		float burst = rand.nextInt(10) / 10f * 0.8f - (3 - player.getBulletBurstLevel()) / 3f;
+		float burst = (float) (rand.nextDouble() * 0.5f + (1 - player.getBulletBurstLevel() / 3f) * 0.5f);
 		if(burst > importance)
 			importance = burst;
 		
-		float score = rand.nextInt(10) / 10f * 0.75f;
+		float score = (float) rand.nextDouble() * 0.8f;
 		if(score > importance)
 			importance = score;
 		
-		float slow = rand.nextInt(10) / 10f * 0.75f - (1 - Enemy.getSlowdown()) / 1f;
+		float slow = (float) (rand.nextDouble() * 0.5f + Enemy.getSlowdown() / 1f * 0.45f);
 		if(slow > importance)
 			importance = slow;
 		
-		float shield = rand.nextInt(10) / 10f * 0.65f - (player.getShields() - 1) / 3;
+		float shield = (float) (rand.nextDouble() * 0.5f + (1 - player.getShields() / 3) * 0.3f);
 		if(shield > importance)
 			importance = shield;
 		
-		float board = rand.nextInt(10) / 10f * 0.7f - gs.getBoardshockCharge(); 
+		float board = (float) (rand.nextDouble() * 0.5f + (1 - gs.getBoardshockCharge()) * 0.5f); 
 		if(board > importance)
 			importance = board;
 		
-		float ricochet = rand.nextInt(10) / 10f * 0.6f;
+		float ricochet = (float) rand.nextDouble() * 0.8f;
 		if(ricochet > importance)
 			importance = ricochet;
 		
-		float pushAway = rand.nextInt(10) / 10f * 0.7f;
+		float pushAway = (float) rand.nextDouble() * 0.8f;
 		if(pushAway > importance)
 			importance = pushAway;
 		
-		float laser = rand.nextInt(10) / 10f * 0.7f * (gs.getPlayer().getAmmoType().equals(AmmoType.LASER) ? 0 : 1);
+		float laser = (float) (rand.nextDouble() * 0.5f + (gs.getPlayer().getAmmoType().equals(AmmoType.LASER) ? 0 : rand.nextDouble()) * 0.5f);
 		if(laser > importance)
 			importance = laser;
 		
