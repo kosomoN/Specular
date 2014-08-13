ee93cdb441031039de0088e2ed0d3475d68d4671
Hugo Holmqvist <hugo.holm@live.com>
2014-08-13 19:54:29 +0300

    Dasher ignores repulsor now.

--- core/src/com/tint/specular/game/powerups/Repulsor.java
+++ core/src/com/tint/specular/game/powerups/Repulsor.java
@@ -10,6 +10,7 @@ import com.tint.specular.game.GameState;
 import com.tint.specular.game.ShockWaveRenderer;
 import com.tint.specular.game.entities.Player;
 import com.tint.specular.game.entities.enemies.Enemy;
+import com.tint.specular.game.entities.enemies.EnemyDasher;
 import com.tint.specular.states.UpgradeState;
 
 
@@ -39,10 +40,12 @@ public class Repulsor extends PowerUp {
 	
 	@Override
 	protected void updatePowerup(Player player) {
+		
 		float distanceSquared;
 		double angle;
 		float playerX = player.getX(), playerY = player.getY(), deltaX = 0, deltaY = 0;
 		for(Enemy e : gs.getEnemies()) {
+			if(!(e instanceof EnemyDasher)){
 			deltaX = e.getX() - playerX;
 			deltaY = e.getY() - playerY;
 			distanceSquared = deltaX * deltaX + deltaY * deltaY;
@@ -50,7 +53,9 @@ public class Repulsor extends PowerUp {
 				angle = Math.atan2(deltaY, deltaX);
 				e.setX((float) (e.getX() + Math.cos(angle) * 20 * (1 - distanceSquared / PUSHAWAY_RANGE)));
 				e.setY((float) (e.getY() + Math.sin(angle) * 20 * (1 - distanceSquared / PUSHAWAY_RANGE)));
+				}
 			}
+	
 		}
 	}
 	
